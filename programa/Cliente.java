572253f685cbe1427b2a22ec2507738bed58363e
Breno Marques <brenosla4@gmail.com>
2025-11-04 02:40:40 -0300

    Funções iniciais e básicas do sistema

---
+++ programa/Cliente.java
@@ -0,0 +1,38 @@
+package Programa;
+
+
+public class Cliente {
+
+    private static int contador = 1;
+    
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Cliente(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        contador++;
+    }
+
+    public static int getContador() {
+    return contador;
+}
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String toString() {
+        return "\nNome: "+nome+"\nCPF: "+cpf+"\nEmail: "+email;
+    }
+}
